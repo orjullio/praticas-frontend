8eebf52ab7e3ba7e9e9e15dcbe9153916d8c9736
Rodrigo Júlio Costa  Silva <13160078+orjullio@users.noreply.github.com>
2022-11-19 00:51:00 -0300

    Início projeto de arrastar figuras.

---
+++ 22 - Arrastar Figuras /script.js
@@ -0,0 +1,26 @@
+const completo = document.querySelector('.completo');
+
+
+function comecarArrastar() {
+    console.log('começar a arrastar')
+}
+
+function terminarArrastar() {
+    console.log('começar a arrastar')
+}
+
+function acimaArrastar() {
+    console.log('começar a arrastar')
+}
+
+function entrarArrastar() {
+    console.log('começar a arrastar')
+}
+
+function sairArrastar() {
+    console.log('começar a arrastar')
+}
+
+function derrubarArrastar() {
+    
+}
